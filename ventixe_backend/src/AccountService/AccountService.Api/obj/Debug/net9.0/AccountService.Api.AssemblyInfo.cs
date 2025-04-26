a775ac8aa252988a57519ca14a78344d4d42bf0e
Nino Hägglund <ninomh99@gmail.com>
2025-04-26 23:13:26 +0200

    Remove Weather api

--- ventixe_backend/src/AccountService/AccountService.Api/obj/Debug/net9.0/AccountService.Api.AssemblyInfo.cs
+++ ventixe_backend/src/AccountService/AccountService.Api/obj/Debug/net9.0/AccountService.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f77d4a7b2ad7b772a44ef5d218a032f4db8782be")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
