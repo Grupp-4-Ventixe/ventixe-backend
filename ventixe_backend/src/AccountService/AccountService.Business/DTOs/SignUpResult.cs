0fe33121957484e2356460e1a4b987dc53fa81ad
Nino Hägglund <ninomh99@gmail.com>
2025-04-30 16:42:16 +0200

    Refactor user management with ASP.NET Core Identity
    - Updated `AuthController` to use `SignUpDto` for user registration.
    
    - Configured `AccountDbContext` and set up Identity services in `Program.cs`.
    
    - Introduced a new SQL Server connection string in `appsettings.json`.
    
    - Enhanced `SignUpDto` with validation attributes.
    
    - Created `AccountEntity` class extending `IdentityUser`.
    
    - Implemented `IAccountRepository` and a base repository class.
    
    - Updated migration files for the new database schema.
    
    - Removed the redundant `DataContext.cs` and
    `AccountService.Domain` project reference.

--- ventixe_backend/src/AccountService/AccountService.Business/DTOs/SignUpResult.cs
+++ ventixe_backend/src/AccountService/AccountService.Business/DTOs/SignUpResult.cs
@@ -6,4 +6,5 @@ public class SignUpResult
 {
     public bool Succeeded { get; set; }
     public List<string> Errors { get; set; } = new();
+    public string? Message { get; set; }
 }
--- ventixe_backend/src/AccountService/AccountService.Data/Repositories/AccountRepository.cs
+++ ventixe_backend/src/AccountService/AccountService.Data/Repositories/AccountRepository.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AccountService.Data.Contexts;
+using AccountService.Data.Entities;
+using AccountService.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AccountService.Data.Repositories;
 
-public class AccountRepository
+public class AccountRepository : BaseRepository<AccountEntity>, IAccountRepository
 {
+    public AccountRepository(AccountDbContext context) : base(context)
+    {
+    }
 
+    public async Task<AccountEntity?> GetUserByIdAsync(string userId)
+    {
+        return await _context.AccountEntities
+            .Where(account => account.UserId == userId)
+            .FirstOrDefaultAsync();
+    }
 }
